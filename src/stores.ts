cbfb9d3621d1ae1316e760692bd1aaba0ba01dec
Rubén <hey@rubenvara.io>
2021-02-17 22:15:40 +0100

    change yearsToLive to be a custom store

--- src/stores.ts
+++ src/stores.ts
@@ -2,5 +2,16 @@ import { Writable, writable } from 'svelte/store';
 
 export const showMenu: Writable<boolean> = writable(false);
 
-export const yearsToLive: Writable<number> = writable(50);
 export const dateString: Writable<string> = writable('1990-07-31');
+
+function createYearsToLive() {
+  const {subscribe, set, update} = writable(60);
+  return {
+    subscribe,
+    // custom methods to make easier to update
+    increment: () => update(n => n + 1),
+    decrement: () => update(n => n - 1),
+    set,
+  };
+}
+export const yearsToLive = createYearsToLive();
