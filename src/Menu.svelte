141501801afb5a77c6c477df7fad77c3181b07f8
Rubén <hey@rubenvara.io>
2022-03-18 14:03:34 +0100

    use svelte animation for menu

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
-  import { showMenu, yearsToLive, dateString } from "./stores";
+  import { fly } from "svelte/transition";
+  import { dateString } from "./stores/date";
+  import { showMenu } from "./stores/menu";
+  import { yearsToLive } from "./stores/years";
 </script>
 
-<aside class:show={$showMenu} class="show">
-  <h1>A visual representation of your life, in weeks</h1>
-  <p>Set your birth date and expected life length.</p>
-  <p>Get how many weeks you have lived, and how many you have left...</p>
-  <fieldset>
-    <div class="date">
-      <label for="birthDate">Date of Birth:</label>
-      <input type="date" name="birthDate" bind:value={$dateString} />
-    </div>
-    <div class="years">
-      <label for="lifeYears">Years to Live</label>
-      <div class="yearsInputs">
-        <button type="button" on:click={yearsToLive.increment}>+</button>
-        <input
-          type="number"
-          name="lifeYears"
-          bind:value={$yearsToLive}
-          min="0"
-          max="100" />
-        <button type="button" on:click={yearsToLive.decrement}>-</button>
+{#if $showMenu}
+  <aside transition:fly={{ x: -250 }}>
+    <h1>A visual representation of your life, in weeks</h1>
+    <p>Set your birth date and expected life length.</p>
+    <p>Get how many weeks you have lived, and how many you have left...</p>
+    <fieldset>
+      <div class="date">
+        <label for="birthDate">Date of Birth:</label>
+        <input type="date" name="birthDate" bind:value={$dateString} />
       </div>
-    </div>
-  </fieldset>
-</aside>
+      <div class="years">
+        <label for="lifeYears">Years to Live</label>
+        <div class="yearsInputs">
+          <button type="button" on:click={yearsToLive.increment}>+</button>
+          <input
+            type="number"
+            name="lifeYears"
+            bind:value={$yearsToLive}
+            min="0"
+            max="100" />
+          <button type="button" on:click={yearsToLive.decrement}>-</button>
+        </div>
+      </div>
+    </fieldset>
+  </aside>
+{/if}
 
-<!-- I made scss work! 🎊 (https://daveceddia.com/svelte-with-sass-in-vscode/) -->
 <style lang="scss">
   aside {
     width: 250px;
@@ -39,35 +43,33 @@
     background: white;
     padding: 12px;
     padding-top: 22px;
-    transform: translateX(-250px);
     box-shadow: 2px 0 5px 5px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s;
     opacity: 0.9;
-    &.show {
-      transform: translateX(0);
+    h1 {
+      font-size: 1.3rem;
+      margin-bottom: 20px;
     }
-  }
-  h1,
-  p {
-    margin-bottom: 20px;
-  }
-  fieldset {
-    padding: 0;
-    margin: 0;
-    margin-top: 40px;
-    border: none;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    > div {
+    p {
+      margin-bottom: 20px;
+    }
+    fieldset {
+      padding: 0;
+      margin: 0;
+      margin-top: 40px;
+      border: none;
       display: flex;
-      align-items: center;
-      justify-content: space-between;
+      flex-direction: column;
+      gap: 20px;
       > div {
         display: flex;
-        flex-direction: column;
-        gap: 12px;
-        max-width: 50px;
+        align-items: center;
+        justify-content: space-between;
+        > div {
+          display: flex;
+          flex-direction: column;
+          gap: 12px;
+          max-width: 50px;
+        }
       }
     }
   }
